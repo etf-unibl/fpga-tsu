1f9e1ec821136fe67e20b63a2c9a5291b878b737
NikolaBabic1213 <nikola.babic@student.etf.unibl.org>
2024-02-09 12:38:40 +0100

    Issue #9 Register map testbench - Updated testbench for reg_map

--- hardware/tests/09/reg_map_tb.vhd
+++ hardware/tests/09/reg_map_tb.vhd
@@ -29,34 +29,27 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;                                
 
-ENTITY reg_map_tb IS
-END reg_map_tb;
-
-ARCHITECTURE reg_map_arch OF reg_map_tb IS
+ENTITY reg_map_vhd_tst IS
+END reg_map_vhd_tst;
+ARCHITECTURE reg_map_arch OF reg_map_vhd_tst IS
 -- constants                                                 
 -- signals                                                   
 SIGNAL clk_i : STD_LOGIC;
-SIGNAL rst_i :  STD_LOGIC;
 SIGNAL ts_high_i : STD_LOGIC_VECTOR(31 DOWNTO 0);
 SIGNAL ts_high_o : STD_LOGIC_VECTOR(31 DOWNTO 0);
 SIGNAL ts_low_i : STD_LOGIC_VECTOR(31 DOWNTO 0);
 SIGNAL ts_low_o : STD_LOGIC_VECTOR(31 DOWNTO 0);
 SIGNAL value_i : STD_LOGIC_VECTOR(31 DOWNTO 0);
 SIGNAL value_o : STD_LOGIC_VECTOR(31 DOWNTO 0);
-SIGNAL we_i    : STD_LOGIC_VECTOR(31 DOWNTO 0);
-SIGNAL we_o    : STD_LOGIC_VECTOR(31 DOWNTO 0);
 COMPONENT reg_map
 	PORT (
 	clk_i : IN STD_LOGIC;
-	rst_i : IN STD_LOGIC;
 	ts_high_i : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
 	ts_high_o : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
 	ts_low_i : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
 	ts_low_o : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
 	value_i : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-	value_o : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-	we_i      : IN  std_logic_vector(31 downto 0);
-	we_o      : OUT  std_logic_vector(31 downto 0)
+	value_o : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
 	);
 END COMPONENT;
 BEGIN
@@ -64,20 +57,13 @@ BEGIN
 	PORT MAP (
 -- list connections between master ports and signals
 	clk_i => clk_i,
-	rst_i => rst_i,
 	ts_high_i => ts_high_i,
 	ts_high_o => ts_high_o,
 	ts_low_i => ts_low_i,
 	ts_low_o => ts_low_o,
 	value_i => value_i,
-	value_o => value_o,
-	we_i => we_i,
-	we_o => we_o
+	value_o => value_o
 	);
-	
- -- stimulus generator for reset
- rst_i <= '1', '0' after 20 ns;	
-
 process
 begin
   for i in 0 to 1000001 loop
@@ -87,57 +73,26 @@ begin
     wait for 10 ns;
   end loop;
 wait;
-end process; 
-
-
-validation_one_reg: PROCESS                                              
+end process;                                      
+always : PROCESS                                              
+-- optional sensitivity list                                  
+-- (        )                                                 
+-- variable declarations                                      
 BEGIN          
   ts_high_i <= (others => '0');  
   ts_low_i <= (others => '0'); 
-
-  wait for 40 ns;               
-
-  for i in 0 to 499999 loop
-	  ts_high_i <= std_logic_vector(to_unsigned(i, 32));
-	  ts_low_i <= std_logic_vector(to_unsigned(i, 32));
-	  value_i <= std_logic_vector(to_unsigned(i, 32));
-	  we_i <= std_logic_vector(to_unsigned(i, 32));
-	  
-	  wait for 20 ns;
-	  assert (value_o = std_logic_vector(to_unsigned(i, 32))) report "Error value; Expected: " & integer'image(i) & " Actual: " & integer'image(to_integer(unsigned(value_o))) severity failure;
-	  assert (we_o = std_logic_vector(to_unsigned(i, 32))) report "Error value; Expected: " & integer'image(i) & " Actual: " & integer'image(to_integer(unsigned(we_o))) severity failure;
-	  
-	  wait for 20 ns;
-	  assert (ts_high_o = std_logic_vector(to_unsigned(i, 32))) report "Error ts_high; Expected: " & integer'image(i) & " Actual: " & integer'image(to_integer(unsigned(ts_high_o))) severity failure;
-	  assert (ts_low_o = std_logic_vector(to_unsigned(i, 32))) report "Error ts_low; Expected: " & integer'image(i) & " Actual: " & integer'image(to_integer(unsigned(ts_low_o))) severity failure;
+  value_i <= (others => '0');   
+  wait for 20 ns;               
+  for i in 0 to 1000000 loop
+  ts_high_i <= std_logic_vector(to_unsigned(i, 32));
+  ts_low_i <= std_logic_vector(to_unsigned(i, 32));
+  value_i <= std_logic_vector(to_unsigned(i, 32));
+  wait for 20 ns;
+  assert (ts_high_o = std_logic_vector(to_unsigned(i, 32))) report "Error ts_high; Expected: " & integer'image(i) & " Actual: " & integer'image(to_integer(unsigned(ts_high_o))) severity failure;
+  assert (ts_low_o = std_logic_vector(to_unsigned(i, 32))) report "Error ts_low; Expected: " & integer'image(i) & " Actual: " & integer'image(to_integer(unsigned(ts_low_o))) severity failure;
+  assert (value_o = std_logic_vector(to_unsigned(i, 32))) report "Error value; Expected: " & integer'image(i) & " Actual: " & integer'image(to_integer(unsigned(value_o))) severity failure;
   end loop;
-  
   report "Test successfully finished.";
 WAIT;                                                        
-END PROCESS validation_one_reg;
-
---validation_two_reg : PROCESS                                              
---BEGIN          
---  ts_high_i <= (others => '0');  
---  ts_low_i <= (others => '0'); 
---  value_i <= (others => '0');   
---  we_i <= (others => '0');
---  
---  wait for 40 ns;               
---  for i in 0 to 1000000 loop
---
---	  value_i <= std_logic_vector(to_unsigned(i, 32));
---	  we_i <= std_logic_vector(to_unsigned(i, 32));
---	  
---	  wait for 20 ns;
---	 
---	  assert (value_o = std_logic_vector(to_unsigned(i, 32))) report "Error value; Expected: " & integer'image(i) & " Actual: " & integer'image(to_integer(unsigned(value_o))) severity failure;
---	  assert (we_o = std_logic_vector(to_unsigned(i, 32))) report "Error value; Expected: " & integer'image(i) & " Actual: " & integer'image(to_integer(unsigned(we_o))) severity failure;
---
---  end loop;
---  
---  report "Test successfully finished.";
---WAIT;                                                        
---END PROCESS validation_two_reg;
-                                          
+END PROCESS always;                                          
 END reg_map_arch;
